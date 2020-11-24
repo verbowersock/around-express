501ec0fb3dd32e172104485914f07b462ecfe543
vermellon <bowernika@gmail.com>
2020-11-24 14:18:17 -0500

    fixed eslint errors

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ const express = require('express');
 const users = require('./routes/users.js');
 const cards = require('./routes/cards.js');
 
-const { PORT = 3000} = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(`${__dirname}/public`));
 
 app.listen(PORT, () => {
-  console.log(`App listening at port ${PORT}`)
-})
+});
 
 app.use('/users', users);
-app.use('/cards', cards)
+app.use('/cards', cards);
 
-app.use(function(req, res) {
-  res.status(404).json({"message" :"Page not found"})
-})
+app.use((req, res) => {
+  res.status(404).json({ message: 'Page not found' });
+});
--- routes/cards.js
+++ routes/cards.js
@@ -6,13 +6,13 @@ module.exports = router;
 
 const filePath = path.join(__dirname, '..', 'data', 'cards.json');
 
-router.get('/', (req, res)=> {
-  fs.readFile(filePath, {encoding: 'utf8'}, (err, data) => {
+router.get('/', (req, res) => {
+  fs.readFile(filePath, { encoding: 'utf8' }, (err, data) => {
     if (err) {
-      res.status(404).json({message: 'Resource not found'});
+      res.status(404).json({ message: 'Resource not found' });
       return;
     }
-     const cards = JSON.parse(data);
-     res.send(cards)
-    })
-  })
+    const cards = JSON.parse(data);
+    res.send(cards);
+  });
+});
